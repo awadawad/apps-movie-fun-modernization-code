0148fbf270dafb513941cf1a244501daf24c4611
Awad Awad <email@awadawad.com>
2017-10-31 10:33:22 -0600

    * completed modernisation (service discovery sub chapter) lab exercises

--- applications/movie-fun-app/src/main/java/org/superbiz/moviefun/ClientConfiguration.java
+++ applications/movie-fun-app/src/main/java/org/superbiz/moviefun/ClientConfiguration.java
@@ -1,6 +1,7 @@
 package org.superbiz.moviefun;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestOperations;
